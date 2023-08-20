1fa37998d8f1fdd86c53257bd9a0e5ad9f81647e
dylan <dylangthomas1@gmail.com>
2023-08-20 14:34:05 -0600

    pretty bad and the js isnt working, but express is going fine

--- routes/newRoute.js
+++ routes/newRoute.js
@@ -1,31 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const messages = [
-    {
-        text: 'Hello World',
-        user: 'Dylan',
-        added: new Date()
-    },
-    {
-        text: 'here comes the pain train',
-        user: 'Tripp',
-        added: new Date()
-    }
-]
-
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Hello Earthlings', messages})
-})
-
-router.get('/new', (req, res) => {
     res.render('form')
 })
 
 router.use(express.urlencoded());
 router.use(express.json());
 
-router.post('/new', (req, res) => {
+router.post('/', (req, res) => {
     console.log(req.body, 'hmm')
     messages.push({
          text: req.body.text,
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = express()
 app.set('view engine', 'ejs')
 
 const indexRoute = require('./routes/indexRoute')
+const newRoute = require('./routes/newRoute')
 
 app.use('/', indexRoute)
+app.use('/new', newRoute)
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
